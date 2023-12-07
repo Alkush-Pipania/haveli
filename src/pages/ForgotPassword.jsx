e998aabac461b37934e92e52161799aa03b23edb
Alkush Pipania <133737222+Alkush-Pipania@users.noreply.github.com>
2023-12-07 20:17:38 +0530

    Update ForgotPassword.jsx

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -25,8 +25,8 @@ export default function ForgotPassword() {
       }
     }
   return (
-    <section >
-        <h1 className="text-3xl text-center mt-6 font-bold">Forgot Password</h1>
+    <section className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500" >
+        <h1 className="text-3xl text-center pt-6 font-bold">Forgot Password</h1>
         <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-[6xl] mx-auto">
             <div className="md:w-[65%] lg:w-[50%] mb-[12px] md:mb-[6px] ">
             <Lottie  animationData={ecom} className="w-full" />
